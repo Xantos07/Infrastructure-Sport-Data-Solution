8fccd82a8a25d9528b8c47bb987e0da4dd07df8b
Xantos07 <coalstudio7@gmail.com>
2026-03-06 18:44:58 +0100

    fix: distance condition

--- notification_discord_consumer.py
+++ notification_discord_consumer.py
@@ -15,10 +15,11 @@ for index, row in enumerate(data):
     if index >= 5:
         break
 
+    distance = row['distance'] if row['distance'] is not None else 0
     embed = {
         ## mettre le bon nom de la personne par rapport à l'id comme dans la consigne 
         "title": f"Nouvelle activité sportive pour l'employé {row['employee_id']}",
-        "description": f"Séance de {row['sport_type']} le {row['start_timestamp'].strftime('%d/%m/%Y à %H:%M')}\n{row['distance']}m en {row['elapsed_time'] / 60:.1f} mins\n\nDétails: {row['details']}",
+        "description": f"Séance de {row['sport_type']} le {row['start_timestamp'].strftime('%d/%m/%Y à %H:%M')}\n{distance}m en {row['elapsed_time'] / 60:.1f} mins\n\nDétails: {row['details']}",
     }
     data = {
     "embeds": [embed]
